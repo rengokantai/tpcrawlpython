654cbf01a074b9ed6e82aa5af539337777248d86
Hernan Y.Ke <yuriqiao@gmail.com>
2015-07-09 00:00:05 -0400

    Add func

--- crawler.py
+++ crawler.py
@@ -9,10 +9,23 @@ class Crawler:
         self.start_url =start_url
         self.depth = depth
         self.apps = []
+        self.current_depth = 0
+        self.depth_links = []
 
 
     def crawl(self):
-        self.get_app_from_link(self.start_url)
+        app=self.get_app_from_link(self.start_url)
+        self.apps.append(app)
+        self.depth_links.append(app.links)
+
+        while self.current_depth < self.depth:
+            current_links =[]
+            for link in self.depth_links[self.current_depth]:
+                current_app = self.get_app_from_link(link)
+                current_links.extend(current_links)
+                self.apps.append(app)
+            self.current_depth+=1
+            self.depth_links.append(current_links)
         return
 
     def get_app_from_link(self, link):
@@ -24,22 +37,23 @@ class Crawler:
         price = tree.xpath('//div[@itemprop="price"]/text()')[0]
         links = tree.xpath('//div[@class="center-stack"]//*/a[@class="name"]/@href')
 
-        app = App(name,developer, price, link)
+        app = App(name,developer, price, links)
 
         self.apps.append(app)
+        self.depth_links.append(app.links)
 
         for link in links:
             print (link)
 
 
         print (name)
-        return
+        return app
 class App:
-    def __init__(self, name,developer, price, link):
+    def __init__(self, name,developer, price, links):
         self.name = name
         self.developer = developer
         self.price = price
-        self.link = link
+        self.links = links
 
     def __str__(self):
         return "name: " + self.name
